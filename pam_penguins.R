421fff8a7383118bd878c919e8537a2fd7eb5669
vitozefferino <115221035+vitozefferino@users.noreply.github.com>
2024-06-20 17:59:20 +0200

    optimal clusters number found

--- pam_penguins.R
+++ pam_penguins.R
@@ -1,6 +1,5 @@
 # Importing required libraries
 library(cluster)
-library(graphics)
 library(factoextra)
 
 # Importing data
@@ -17,8 +16,13 @@ penguins <- na.omit(penguins)
 clean_penguins <- penguins[penguins$flipper_length_mm < 4000 & penguins$flipper_length_mm > 0, ]
 boxplot(clean_penguins)
 
-# Detecting the optimal number of clusters 'k'
+# Dataframe without factor columns
 clean_penguins_numeric <- clean_penguins[, sapply(clean_penguins, is.numeric)]
+
+# Checking the optimal number of clusters with wss
+fviz_nbclust(clean_penguins_numeric, pam, method = "wss")
+
+# Checking the optimal number of clusters with silhouette
 fviz_nbclust(clean_penguins_numeric, pam, method = "silhouette")
 
 # PAM algorithm
@@ -27,7 +31,7 @@ pam.out
 plot(clean_penguins, col =(pam.out$cluster+1), main="PAM (K=2)", pch=19)
 points(pam.out$medoids, pch=as.character(pam.out$cluster[pam.out$id.med]))
 
-# TODO Which plot to show
+# TODO Significant plots to show
 
 # plot(clean_penguins$flipper_length_mm, clean_penguins$bill_depth_mm, 
 #      col =(pam.out$cluster+1), 
