56077f81eb8f0fdb2a477337918294d9b97288c9
steezy__ <115320376+RoccoCode13@users.noreply.github.com>
2024-06-21 23:34:53 +0200

    Update dbscan

--- pam_penguins.R
+++ pam_penguins.R
@@ -175,3 +175,67 @@ plot(clean_penguins$flipper_length_mm, clean_penguins$body_mass_g,
 points(pam.out$medoids[, c("flipper_length_mm", "body_mass_g")], 
        pch=as.character(pam.out$cluster[pam.out$id.med]), 
        col="black")
+
+
+#Clustering con DBSCAN
+library(dbscan)
+#Seleziono le variabili numeriche
+penguins_num <- penguins %>% select(culmen_length_mm, culmen_depth_mm, flipper_length_mm, body_mass_g)
+
+# Standardizzo i dati
+penguins_scaled <- scale(penguins_num)
+
+# Determino il valore di eps usando il diagramma k-dist
+kNNdistplot(penguins_scaled, k = 4)
+abline(h=0.8, col = "red", lty=8)
+
+# Applico DBSCAN con eps e minPts scelti
+dbscan_result <- dbscan(penguins_scaled, eps = 0.8, minPts = 5)
+dbscan_result
+
+# abbiamo 2 cluster ed un terzo con 4 punti rumorosi
+# questi punti non appartengono a nessun cluster denso e possono essere considerati anomalie
+
+# Aggiungo i cluster al dataset originale
+penguins$cluster <- factor(dbscan_result$cluster)
+
+# Posso ottenere il numero di punti per ogni cluster anche in questo modo
+table(penguins$cluster)
+
+# Analizzo le caratteristiche dei cluster
+# facendo una tabella con le medie delle variabili per ciascun cluster
+penguins %>% group_by(cluster) %>% summarise(
+  culmen_length_mm = mean(culmen_length_mm, na.rm = TRUE),
+  culmen_depth_mm = mean(culmen_depth_mm, na.rm = TRUE),
+  flipper_length_mm = mean(flipper_length_mm, na.rm = TRUE),
+  body_mass_g = mean(body_mass_g, na.rm = TRUE)
+)
+# i pinguini con una massa corporea maggiore tendono ad avere il becco piu lungo e meno spesso
+# quelli meno pesanti ad avere il becco piu corto e spesso
+# la media dell'ala del cluster "rumoroso" e' nettamente maggiore dei altri due claster per colpa di un outlier
+
+# Visualizzo i cluster
+fviz_cluster(dbscan_result, data = penguins_scaled, geom = "point", ellipse = FALSE, show.clust.cent = FALSE) + theme_minimal()
+# la visualizzazione non e' ottima per colpa dei punti rumorosi
+
+# Filtro i punti rumorosi
+penguins_scaled_filtered <- penguins_scaled[penguins$cluster != 0, ]
+
+# Applico nuovamente DBSCAN sul dataframe filtrato (senza i punti rumorosi)
+dbscan_result_filtered <- dbscan(penguins_scaled_filtered, eps = 0.8, minPts = 5)
+dbscan_result_filtered
+
+# Visualizzo i cluster senza i punti rumorosi
+fviz_cluster(dbscan_result_filtered, data = penguins_scaled_filtered, geom = "point", ellipse = FALSE, show.clust.cent = FALSE) + theme_minimal()
+# la prima dimensione ha una variabilita' dei dati maggiore rispetto alla seconda
+
+
+# Scatter plot per vedere i cluster identificati
+ggplot(penguins, aes(x = culmen_length_mm, y = culmen_depth_mm, color = cluster)) + geom_point() + theme_minimal()
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, color = cluster)) + geom_point() + theme_minimal()
+#notiamo 2 un outlier ovvero:
+#una lunghezza negativa ed una sproporzionata(5k mm)
+
+# filtrando:
+ggplot(penguins_filtered, aes(x = flipper_length_mm, y = body_mass_g, color = cluster)) + geom_point() + theme_minimal()
+# all'aumentare del peso corporeo aumenta anche la lunghezza delle ali
